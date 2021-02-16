c9bc5e52e4aa8a157911d30c8b32540671f32bef
rohguentak <rhgt78@gmail.com>
2021-02-17 03:37:37 +0900

    add user screen have bio form

--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
 class User(AbstractUser):
 
-    pass
+    bio = models.TextField(default="")
