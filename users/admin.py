3345581f1b07616926cffcf31718c2ae00481b39
rohguentak <rhgt78@gmail.com>
2021-02-17 04:12:14 +0900

    add filter box

--- users/admin.py
+++ users/admin.py
@@ -7,4 +7,14 @@ from . import models
 
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    """ Custom User Admin """
+
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = (
+        "language",
+        "currency",
+        "superhost",
+    )
+
+
+# admin.site.register(models.User, CustomUserAdmin)
