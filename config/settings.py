08aa225120f6b71b3b04b46d47c3d0ce681dbc25
rohguentak <rhgt78@gmail.com>
2021-02-17 20:45:08 +0900

    add core app for timestamp and django_countries

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,12 @@ DJANGO_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
-THIRD_PARTY_APPS = []
-PROJECT_APPS = ["users.apps.UsersConfig", "rooms.apps.RoomsConfig"]
+THIRD_PARTY_APPS = ["django_countries"]
+PROJECT_APPS = [
+    "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
+    "core.apps.CoreConfig",
+]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
