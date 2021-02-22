8c30582fb192eea1ab7c4687aa0a4596cbcd1739
rohguentak <rhgt78@gmail.com>
2021-02-23 00:17:45 +0900

    make reservation admin pannel

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,11 +1,61 @@
 from django.contrib import admin
+from django.utils import timezone
 from . import models
 
 # Register your models here.
+class ProgressListFilter(admin.SimpleListFilter):
+    title = "In Progress"
+    parameter_name = "in_progress"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("True", "True"),
+            ("False", "False"),
+        )
+
+    def queryset(self, request, queryset):
+        now = timezone.now().date()
+
+        if self.value() == "True":
+            return queryset.filter(check_in__lt=now, check_out__gt=now)
+        elif self.value() == "False":
+            return queryset.exclude(check_in__lt=now, check_out__gt=now)
+
+
+class FinishedListFilter(admin.SimpleListFilter):
+    title = "Is Finished"
+    parameter_name = "is_finished"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("True", "True"),
+            ("False", "False"),
+        )
+
+    def queryset(self, request, queryset):
+        now = timezone.now().date()
+        if self.value() == "True":
+            return queryset.filter(check_out__gt=now)
+        elif self.value() == "False":
+            return queryset.exclude(check_out__gt=now)
 
 
 @admin.register(models.Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     """reservation admin definition """
 
-    pass
+    list_display = (
+        "room",
+        "status",
+        "check_in",
+        "check_out",
+        "guest",
+        "in_progress",
+        "is_finished",
+    )
+
+    list_filter = (
+        "status",
+        ProgressListFilter,
+        FinishedListFilter,
+    )
--- reservations/models.py
+++ reservations/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.dateparse import parse_date
 from core import models as core_models
 
 # Create your models here.
@@ -31,3 +33,19 @@ class Reservation(core_models.TimeStampedModel):
 
     def __str__(self):
         return f"{self.room} - {self.check_in}"
+
+    def get_now_date():
+        now = timezone.now().date()
+        return now
+
+    def in_progress(self):
+        now = timezone.now().date()
+        return now >= self.check_in and now < self.check_out
+
+    in_progress.boolean = True
+
+    def is_finished(self):
+        now = timezone.now().date()
+        return now >= self.check_out
+
+    is_finished.boolean = True
