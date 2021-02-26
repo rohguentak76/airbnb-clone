20a83ea7cd36180bb401755eb786bf11e1b517fd
rohguentak <rhgt78@gmail.com>
2021-02-26 21:25:15 +0900

    .page() can control page more than get_page

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,16 @@
 from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
+
 from . import models
 
 # Create your views here.
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    paginator = Paginator(room_list, 10, orphans=2)
+    try:
+        rooms_page = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"pages": rooms_page})
+    except EmptyPage:
+        return redirect("/")
